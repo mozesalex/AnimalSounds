8a5af98ed5f14a497d886417bc8d4152400ad410
mozesalex <137737558+mozesalex@users.noreply.github.com>
2025-03-16 15:44:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Interfata pentru animale
+interface IAnimal
+{
+    string Name { get; }
+    string MakeSound();
+}
+
+// Clase pentru diferite tipuri de animale
+class Dog : IAnimal
+{
+    public string Name => "Dog";
+    public string MakeSound() => "Bark";
+}
+
+class Cat : IAnimal
+{
+    public string Name => "Cat";
+    public string MakeSound() => "Meow";
+}
+
+class Cow : IAnimal
+{
+    public string Name => "Cow";
+    public string MakeSound() => "Moo";
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<IAnimal> animals = new List<IAnimal>
+        {
+            new Dog(),
+            new Cat(),
+            new Cow()
+        };
+
+        foreach (var animal in animals)
+        {
+            Console.WriteLine($"{animal.Name} makes sound: {animal.MakeSound()}");
+        }
+    }
+}
